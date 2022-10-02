933669ff1a4759a43d554684958a4731f8f3e848
VolgnaGath <dwyane2009@yandex.ru>
2022-10-03 00:27:39 +0300

    1

---
+++ src/Lesson5/Numbers.java
@@ -0,0 +1,28 @@
+package Lesson5;
+
+public class Numbers {
+    private int column1;
+    private int column2;
+    private int column3;
+
+    public int getColumn1() {
+        return column1;
+    }
+
+    public int getColumn2() {
+        return column2;
+    }
+
+    public int getColumn3() {
+        return column3;
+    }
+
+    public Numbers(int column1, int column2, int column3) {
+        this.column1 = column1;
+        this.column2 = column2;
+        this.column3 = column3;
+    }
+
+
+
+}
